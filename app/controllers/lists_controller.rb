fb851e9e78a9ff930d94cebc8df2181a4928112d
t-akei <145541870+t-akei@users.noreply.github.com>
2023-10-18 13:21:39 +0000

    [Finish]3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,7 +8,7 @@ class ListsController < ApplicationController
     list.save
 
     redirect_to list_path(list.id)
-  
+
   end
 
   def index
@@ -22,13 +22,17 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
   private
   def list_params
     params.require(:list).permit(:title, :body, :image)
