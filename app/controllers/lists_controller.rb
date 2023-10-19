aaf339c6efb25fec589ac3b4a88500e164ce783b
t-akei <145541870+t-akei@users.noreply.github.com>
2023-10-19 00:01:36 +0000

    [Finish]3-9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,7 +8,7 @@ class ListsController < ApplicationController
     if @list.save
       redirect_to list_path(@list.id)
     else
-      render :new
+      redirect_to new_list_path
     end
   end
 
